b08970b88adb363ddd893b884daf9502941c58ad
Kevin Martin <kevin.martin.albert@gmail.com>
2024-07-15 20:16:44 +0200

    ocr 1-3 annexe

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/";
-import Survey from "./pages/Survey";
+import Survey from "./pages/Survey/Survey"; // Corrected import path
 import Header from "./components/Header";
+import ClientForm from "./components/ClientForm";
+import FreelanceForm from "./components/FreelanceForm";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +13,11 @@ ReactDOM.render(
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/survey" element={<Survey />} />
+        <Route path="/survey" element={<Survey />}>
+          {/* Nous imbriquons nos composants dans survey */}
+          <Route path="client" element={<ClientForm />} />
+          <Route path="freelance" element={<FreelanceForm />} />
+        </Route>
       </Routes>
     </Router>
   </React.StrictMode>,
